7189a7213679d1b6fe37c322564327bf68fcf849
domi <491431567@qq.com>
2020-11-22 23:10:50 +0800

    fix

--- src/main/java/org/note/function/middleware/redis/RedisClientConfig.java
+++ src/main/java/org/note/function/middleware/redis/RedisClientConfig.java
@@ -15,43 +15,43 @@ public class RedisClientConfig {
     @Bean
     public RedissonClient redisClient2() {
         Config config = new Config();
-        setConfig(config);
+//        setConfig(config);
         setClusterServers(config);
         return Redisson.create(config);
     }
 
     private void setConfig(Config config) {
-        config.setCodec(null);
-        config.setAddressResolverGroupFactory(null);
+//        config.setCodec(null);
+//        config.setAddressResolverGroupFactory(null);
         config.setCleanUpKeysAmount(100);
-        config.setEventLoopGroup(null);
-        config.setExecutor(null);
+//        config.setEventLoopGroup(null);
+//        config.setExecutor(null);
         // 保持订阅发布顺序 默认true
         // 通过该参数来修改是否按订阅发布消息的接收顺序出来消息，
         // 如果选否将对消息实行并行处理，该参数只适用于订阅发布消息的情况。
         config.setKeepPubSubOrder(false);
-        config.setLockWatchdogTimeout(15L);
-        config.setMaxCleanUpDelay(11);
-        config.setMinCleanUpDelay(11);
-        config.setNettyHook(null);
-        config.setNettyThreads(11);
+        config.setLockWatchdogTimeout(1500);
+        config.setMaxCleanUpDelay(1000);
+        config.setMinCleanUpDelay(1000);
+//        config.setNettyHook(null);
+        config.setNettyThreads(5);
         config.setReferenceEnabled(false);
         config.setUseThreadClassLoader(false);
-        config.setTransportMode(null);
+//        config.setTransportMode(null);
         config.setUseScriptCache(false);
     }
 
     private void setClusterServers(Config config) {
         ClusterServersConfig clusterConfig = config.useClusterServers();
-        clusterConfig.addNodeAddress("redis://127.0.0.1:7000", "redis://127.0.0.1:7001");
-        clusterConfig.setMasterConnectionPoolSize(20);
+        clusterConfig.addNodeAddress("redis://192.168.15.128:7001", "redis://192.168.15.128:7002","redis://192.168.15.128:7003", "redis://192.168.15.128:7004", "redis://192.168.15.128:7005", "redis://192.168.15.128:7006");
         clusterConfig.setScanInterval(2000);
-        clusterConfig.setIdleConnectionTimeout(10);
-        clusterConfig.setConnectTimeout(23);
-        clusterConfig.setTimeout(12);
-        clusterConfig.setRetryAttempts(3);
-        clusterConfig.setReadMode(ReadMode.SLAVE);
-        clusterConfig.setLoadBalancer(new RandomLoadBalancer());
+//        clusterConfig.setMasterConnectionPoolSize(64);
+//        clusterConfig.setIdleConnectionTimeout(3000);
+//        clusterConfig.setConnectTimeout(10000);
+//        clusterConfig.setTimeout(3000);
+//        clusterConfig.setRetryAttempts(3);
+//        clusterConfig.setReadMode(ReadMode.SLAVE);
+//        clusterConfig.setLoadBalancer(new RandomLoadBalancer());
     }
 
 
