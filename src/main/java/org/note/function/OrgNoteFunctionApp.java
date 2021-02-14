77fcd8370ad66ad1302edcd95e9dae79aad8c904
domi <491431567@qq.com>
2021-02-14 14:06:07 +0800

    熔断测试

--- src/main/java/org/note/function/OrgNoteFunctionApp.java
+++ src/main/java/org/note/function/OrgNoteFunctionApp.java
@@ -3,7 +3,11 @@ package org.note.function;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+@EnableHystrix
+@EnableEurekaClient
 @SpringBootApplication
 @MapperScan(basePackages = {"org.note.function.mapper"})
 public class OrgNoteFunctionApp {
